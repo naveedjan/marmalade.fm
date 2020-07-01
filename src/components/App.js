162cbebcf0cb67ba1bd089491ba87a5303735ba4
Naveed Janmohamed <naveed.janmohamed@icloud.com>
2020-07-01 17:43:32 +0100

    Added pause/play control and state to App

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,6 @@
-import React from "react";
+/*global Mixcloud*/
+
+import React, { Component } from "react";
 
 import FeaturedMix from "./FeaturedMix";
 import Header from "./Header";
@@ -9,35 +11,93 @@ const Home = () => <h1>Home</h1>;
 const Archive = () => <h1>Archive</h1>;
 const About = () => <h1>About</h1>;
 
-function App() {
-  return (
-    <Router>
-      <div>
-        <div className="flex-l justify-end">
-          <FeaturedMix />
-          <div className="w-50-l relative z-1">
-            <Header />
-            <Route exact path="/">
-              <Home />
-            </Route>
-            <Route path="/archive">
-              <Archive />
-            </Route>
-            <Route path="/about">
-              <About />
-            </Route>
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      playing: false,
+      currentMix: "",
+    };
+  }
+
+  mountAudio = async () => {
+    this.widget = Mixcloud.PlayerWidget(this.player);
+    await this.widget.ready;
+    // await this.widget.play();
+    this.widget.events.pause.on(() =>
+      this.setState({
+        playing: false,
+      })
+    );
+    this.widget.events.play.on(() =>
+      this.setState({
+        playing: true,
+      })
+    );
+
+    console.log(this.widget);
+  };
+
+  componentDidMount() {
+    this.mountAudio();
+  }
+
+  togglePlay = () => {
+    this.widget.togglePlay();
+  };
+
+  playMix = (mixName) => {
+    this.widget.load(mixName, true);
+  };
+
+  render() {
+    return (
+      <Router>
+        <div>
+          <div className="flex-l justify-end">
+            <FeaturedMix />
+            <div className="w-50-l relative z-1">
+              <Header />
+
+              <div>
+                <button onClick={this.togglePlay}>
+                  {this.state.playing ? "Pause" : "Play"}
+                </button>
+              </div>
+
+              <div>
+                <button
+                  onClick={() =>
+                    this.playMix("/NTSRadio/bonobo-24th-june-2015/")
+                  }
+                >
+                  Play Mix
+                </button>
+              </div>
+
+              <Route exact path="/">
+                <Home />
+              </Route>
+              <Route path="/archive">
+                <Archive />
+              </Route>
+              <Route path="/about">
+                <About />
+              </Route>
+            </div>
           </div>
+          <iframe
+            width="100%"
+            height="60"
+            src="https://www.mixcloud.com/widget/iframe/?hide_cover=1&mini=1&feed=%2FNTSRadio%2Ffloating-points-jamie-xx-18th-august-2016%2F"
+            frameBorder="0"
+            className="db fixed bottom-0 z-5"
+            ref={(player) => (this.player = player)}
+          ></iframe>
         </div>
-        <iframe
-          width="100%"
-          height="60"
-          src="https://www.mixcloud.com/widget/iframe/?hide_cover=1&mini=1&feed=%2FNTSRadio%2Ffloating-points-jamie-xx-18th-august-2016%2F"
-          frameBorder="0"
-          className="db fixed bottom-0 z-5"
-        ></iframe>
-      </div>
-    </Router>
-  );
+      </Router>
+    );
+  }
 }
 
 export default App;
