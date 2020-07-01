e0f56156d033b0788cb881ba0bcd33b3d4c88036
Naveed Janmohamed <naveed.janmohamed@icloud.com>
2020-07-01 14:12:05 +0100

    Bare bones components

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,23 @@
 import React from "react";
+import FeaturedMix from "./FeaturedMix";
+import Header from "./Header";
 
 function App() {
   return (
     <div className="App">
-      <h1>Marmalade.fm</h1>
+      <div>
+        <FeaturedMix />
+        <div>
+          <Header />
+          {/* routed */}
+        </div>
+      </div>
+      <iframe
+        width="100%"
+        height="60"
+        src="https://www.mixcloud.com/widget/iframe/?hide_cover=1&mini=1&feed=%2FNTSRadio%2Ffloating-points-jamie-xx-18th-august-2016%2F"
+        frameBorder="0"
+      ></iframe>
     </div>
   );
 }
---
+++ src/components/FeaturedMix.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const FeaturedMix = (props) => (
+  <div>
+    <p>Featured mix</p>
+    <h1>Mint Condition w/ Hotthobo - 27th November 2017</h1>
+  </div>
+);
+
+export default FeaturedMix;
