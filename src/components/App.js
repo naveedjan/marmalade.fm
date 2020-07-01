0add7ddd63d4925309e4ff4d216d0a00978a4350
Naveed Janmohamed <naveed.janmohamed@icloud.com>
2020-07-01 15:23:45 +0100

    Added react router

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,42 @@
 import React from "react";
+
 import FeaturedMix from "./FeaturedMix";
 import Header from "./Header";
 
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
+const Home = () => <h1>Home</h1>;
+const Archive = () => <h1>Archive</h1>;
+const About = () => <h1>About</h1>;
+
 function App() {
   return (
-    <div className="App">
+    <Router>
       <div>
-        <FeaturedMix />
-        <div>
-          <Header />
-          {/* routed */}
+        <div className="flex-l justify-end">
+          <FeaturedMix />
+          <div className="w-50-l relative z-1">
+            <Header />
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/archive">
+              <Archive />
+            </Route>
+            <Route path="/about">
+              <About />
+            </Route>
+          </div>
         </div>
+        <iframe
+          width="100%"
+          height="60"
+          src="https://www.mixcloud.com/widget/iframe/?hide_cover=1&mini=1&feed=%2FNTSRadio%2Ffloating-points-jamie-xx-18th-august-2016%2F"
+          frameBorder="0"
+          className="db fixed bottom-0 z-5"
+        ></iframe>
       </div>
-      <iframe
-        width="100%"
-        height="60"
-        src="https://www.mixcloud.com/widget/iframe/?hide_cover=1&mini=1&feed=%2FNTSRadio%2Ffloating-points-jamie-xx-18th-august-2016%2F"
-        frameBorder="0"
-      ></iframe>
-    </div>
+    </Router>
   );
 }
 
